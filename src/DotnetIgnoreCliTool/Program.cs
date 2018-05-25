e587622c461e320920d9cf33b3e70565242f7e5b
Rafał Araszkiewicz <rafal.araszkiewicz@setapp.pl>
2018-05-25 14:39:56 +0200

    Adds project template

---
+++ src/DotnetIgnoreCliTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetIgnoreCliTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
